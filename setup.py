0b8ee033352ca50b1c9aef037d9e4efcfe9de074
Dharhas Pothina <dharhas@gmail.com>
2024-01-13 09:25:12 -0600

    change dependency from opencv-python to opencv

--- setup.py
+++ setup.py
@@ -1,32 +1,34 @@
 import setuptools
+
 from brisque import __version__
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="brisque", # Replace with your own username
+    name="brisque",  # Replace with your own username
     version=__version__,
     author="Rehan Guha",
     py_modules=["brisque"],
-    license='mit',
+    license="mit",
     author_email="rehanguha29@gmail.com",
     description="Image Quality",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rehanguha/brisque",
-    package_dir={'brisque': 'brisque'},
+    package_dir={"brisque": "brisque"},
     packages=setuptools.find_packages(),
     include_package_data=True,
-    keywords = ['quality', 'svm', 'image', 'maths'],
+    keywords=["quality", "svm", "image", "maths"],
     classifiers=[
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python",
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Scientific/Engineering",
-        'Intended Audience :: Developers',
+        "Intended Audience :: Developers",
     ],
-    python_requires='>=2.7',
-    install_requires=['numpy', 'scikit-image', 'scipy', 'opencv-python', 'libsvm'],
+    python_requires=">=2.7",
+    install_requires=["numpy", "scikit-image", "scipy", "opencv", "libsvm"],
 )
